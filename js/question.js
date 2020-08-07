ffb21ec3653d41dcd92421e642592c66cc391b0f
Neelesh Ramane <59531944+neeleshramane@users.noreply.github.com>
2020-08-07 17:39:57 +0530

    First

---
+++ js/question.js
@@ -0,0 +1,79 @@
+
+const quiz = [
+
+    
+    {
+        q: 'According to Ancient Mythology, who was the messenger of the gods?',
+        options: ['Olympus','Hermes','Cleopatra','Achilles'],
+        answer:1
+    },
+
+    {
+        q: 'Neil Alden Armstrong was an American astronaut and aeronautical engineer and the first person to walk on the Moon.Who else walked on the moon?',
+        options: ['Buzz Aldrin','Charles Pete Conrad','None from the options','Both Buzz and Charles'],
+        answer:3
+    },
+
+    {
+        q: 'In chess, the Queen has the combined movement of which pieces?',
+        options: ['Both Bishop and Knight','Both Bishop and Elephant','Bishop, Knight and Elephant','None from the options'],
+        answer:1
+    },
+    {
+        q: 'From the Nursery rhymes,what did the -Three Little Kittens- lose?',
+        options: ['chickens','their rats','mittens','buttons'],
+        answer:2
+    },
+    {
+        q: 'Which WWE Superstar & Hollywood Actor called himself The Great-one and The Peoples Champ in WWE?',
+        options: ['John Cena','Dwayne-The Rock Johnson','Undertaker','The Great Khali'],
+        answer:1
+    },
+    {
+        q: 'Who is often called the father of the computer?',
+        options: ['Bill Gates','J.W. Wills','Charles Babbage','Aristotle'],
+        answer:2
+    },
+    {
+        q: 'Which planet is the hottest in the solar system?',
+        options: ['Mercury','Venus','Saturn','Jupiter'],
+        answer:1
+    },
+    {
+        q: 'Which company owns Bugatti, Audi, Porsche, and Ducati?',
+        options: ['Lamborghini International','Rolls-Royce Motor Cars','Volkswagen Company','Mazda Ltd.'],
+        answer:2
+    },
+    {
+        q: 'Which country invented tea?',
+        options: ['India','China','Great Britain','Mongolia'],
+        answer:1
+    },
+    {
+        q: 'Which bone are babies born without?',
+        options: ['Collar','Knee Cap','Femur','Ankle'],
+        answer:1
+    },
+    {
+        q: 'Which original Avenger was not in the first few movies?',
+        options: ['The Wasp','Incredible Hulk','The Black Panther','The Ant-Man'],
+        answer:0
+    },
+
+    {
+        q: 'Inside which HTML element do we put the JavaScript?',
+        options: ['script','javascript','js','scripting'],
+        answer:0
+    },
+    {
+        q: 'Which fast food stall do Raghu(Sanjay Dutt) and Dedh-Footiya run in the film - Vaastav?',
+        options: ['Panipuri','Pav Bhaji','Vada Pav','Sandwich'],
+        answer:1
+    },
+    {
+        q: 'Everything is going to be ___________. ',
+        options: ['alwright','all right','allright','alright'],
+        answer:3
+    }
+
+]
